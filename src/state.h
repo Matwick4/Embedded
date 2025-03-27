9061217978d8f5c6c62baca538fa8b68a7a1e451
Lorenzo Cortese <lorenzo.cortese@studenti.unitn.it>
2025-03-27 16:31:56 +0100

    added menu files and state file

---
+++ src/state.h
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <cstdint>
+//#include "ti/grlib/grlib.h"
+#ifndef _STATE_H_
+#define _STATE_H_
+
+//TODO CHECK FOR THESE ONES 
+#define J_UP_TRESH 10000
+#define J_DOWN_TRESH 6000
+#define J_RIGHT_TRESH 10000
+#define J_LEFT_TRESH 6000
+#define J_H_INITIAL 8000
+#define J_V_INITIAL 8000
+
+/*
+typedef enum
+{
+    DARK,
+    LIGHT,
+    AUTO
+} brightnessType;
+*/
+struct GameState
+{
+    int game_selected;
+    bool buttonClicked;
+
+    uint16_t joystickX;
+    uint16_t joystickY;
+
+
+    //Graphics_Context gContext;
+    //brightnessType brightness;
+    
+};
+
+GameState gameState;
+
+#endif

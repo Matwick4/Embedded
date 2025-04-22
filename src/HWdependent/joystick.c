fdd02b1e86de30b651fa0dff4e5abf27d899d144
Lorenzo Cortese <lorenzo.cortese@studenti.unitn.it>
2025-04-22 16:59:53 +0200

    Added user buttons read

--- src/HWdependent/joystick.c
+++ src/HWdependent/joystick.c
@@ -1,5 +1,17 @@
 #include <ti/devices/msp432p4xx/inc/msp432p401r.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
 void readJoystickPosition()
 {
     ADC14->CTL0 |= ADC14_CTL0_SC;
+}
+
+bool isButtonUpPressed()
+{
+    return (GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1) == GPIO_INPUT_PIN_LOW);
+}
+
+bool isButtonDownPressed()
+{
+    return (GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN4) == GPIO_INPUT_PIN_LOW);
 }

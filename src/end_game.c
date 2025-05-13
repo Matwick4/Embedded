72115dcf49b09f042c91adc06681213d787beb39
Lorenzo Cortese <lorenzo.cortese@studenti.unitn.it>
2025-05-13 10:19:25 +0200

    added unique endgame screen for both games

--- src/end_game.c
+++ src/end_game.c
@@ -2,9 +2,28 @@
 #include "state.h"
 #include "HWdependent/display.h"
 
-void show_end_game_screen(bool won){
-    clear_Display();
-    draw_String_Centered((won? "Congrats! Yow Won": "Sorry! You lost!"),DISPLAY_WIDTH/2,20,false);
-    //TODO vedere se aggiungere buzzer. Andrebbe messo include relativo nel .h
-    drawStringCentered("Press S1 to continue",DISPLAY_WIDTH/2,80,false);
+void show_end_game_screen(bool won,int player){
+    
+    if(player == -1)
+    {
+        clear_Display();
+        draw_String_Centered((won? "Congrats! Yow Won": "Sorry! You lost!"),DISPLAY_WIDTH/2,20,false);
+        //TODO vedere se aggiungere buzzer. Andrebbe messo include relativo nel .h
+        drawStringCentered("Press S1 to continue",DISPLAY_WIDTH/2,80,false);
+    }
+    else
+    {
+        if(player == 1){
+            clear_Display();
+            draw_String_Centered((won? "Congrats! Player 1 Won"),DISPLAY_WIDTH/2,20,false);
+            //TODO vedere se aggiungere buzzer. Andrebbe messo include relativo nel .h
+            drawStringCentered("Press S1 to continue",DISPLAY_WIDTH/2,80,false);
+        }
+        else{
+            clear_Display();
+            draw_String_Centered((won? "Congrats! Player 2 Won"),DISPLAY_WIDTH/2,20,false);
+            //TODO vedere se aggiungere buzzer. Andrebbe messo include relativo nel .h
+            drawStringCentered("Press S1 to continue",DISPLAY_WIDTH/2,80,false);
+        }
+    }
 }

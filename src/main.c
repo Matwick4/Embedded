e4672f9c26c15ad93b30d36d02dea4b20c15b8f6
Lorenzo Cortese <129780161+TheDeadGuy77@users.noreply.github.com>
2025-05-29 00:00:03 +0200

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include "src/snake.h"
+#include "src/pong.h"
+#include "src/HWdependent/pcm.h"
+#include "src/HWdependent/hw_init.h"
+#include "src/menu.h"
+#include "src/state.h"
+#include "src/end_game.h"
+
+
+
+int main(void)
+{
+    gameState.buttonClicked = false;
+    gameState.joystickY = J_V_INITIAL;
+    gameState.joystickX = J_H_INITIAL;
+
+    HW_Init();
+    while(true)
+    {
+        gameState.game_selected = 0;
+        show_main_menu();
+        int winner_pong;
+        bool winner_snake;
+        switch(gameState.game_selected){
+            case 0: 
+                winner_pong = pong();
+                show_end_game_screen(true,winner_pong);
+            break;
+
+            case 1: 
+                winner_snake = snake();
+                show_end_game_screen(winner_snake,-1);
+            break;
+
+            default:
+                winner_snake = false;
+                winner_pong = -1;
+            break;
+        }
+        gotoLPM();
+        
+    }
+}
